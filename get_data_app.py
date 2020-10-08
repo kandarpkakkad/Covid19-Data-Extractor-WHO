00630d00de7796bc0d576d60e407a80e391a52c7
Kandarp Kakkad <kandarp.b.kakkad@gmail.com>
2020-10-08 17:35:40 +0530

    Commit Three

--- get_data_app.py
+++ get_data_app.py
@@ -6,10 +6,14 @@ from extract_covid_cases import extract
 app = Flask(__name__)
 
 
+states = ['Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Ladakh', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal']
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    global states
     if request.method == "GET":
-        return render_template("home.html")
+        return render_template("home.html", data=states)
     else:
         data, districts = extract(request.form['state'])
         return render_template("home.html")

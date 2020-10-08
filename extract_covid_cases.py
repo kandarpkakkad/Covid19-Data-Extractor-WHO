0b36b5a8af0e8461ba0473ace351ddca4a75ba7c
Kandarp Kakkad <kandarp.b.kakkad@gmail.com>
2020-10-08 13:03:16 +0530

    Commit Two

--- extract_covid_cases.py
+++ extract_covid_cases.py
@@ -1,6 +1,24 @@
 import pandas as pd
 
-data2 = pd.DataFrame(pd.read_csv("https://api.covid19india.org/csv/latest/districts.csv", header=None, usecols=[0, 1, 2, 3, 4, 5, 7], low_memory=False))
-data2_guj = data2.loc[(data2[1] == "Gujarat") & (data2[2] == "Rajkot")]
 
-print(data2_guj)
+def extract(state: str) -> (pd.DataFrame, list):
+    """
+    :param state: Name of the state for extraction of the data for the state
+
+    Getting data of respective state and its districts date wise.
+
+    :return: Dataframe of data of respective state
+    """
+    try:
+        assert isinstance(state, str)
+    except AssertionError as _:
+        print("String Needed")
+        raise
+    print("Reading Data for {}".format(state))
+    # Read CSV file from covid19india API
+    data_districts = pd.DataFrame(pd.read_csv("districts.csv", header=None, usecols=[0, 1, 2, 3, 4, 5, 7]))
+    # Get data of respective state
+    data_districts_state = data_districts.loc[(data_districts[1] == state)]
+    districts = data_districts_state[2].unique()
+    print(data_districts_state, districts)
+    return data_districts_state, districts
--- get_data_app.py
+++ get_data_app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
+from extract_covid_cases import extract
+
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html")
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        data, districts = extract(request.form['state'])
+        return render_template("home.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port='8080')

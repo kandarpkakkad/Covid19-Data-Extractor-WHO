22b094db3fe036ef2d721411cbe0c98df7394e01
Harsh <harshchaplot11@gmail.com>
2020-10-09 10:07:27 +0530

    Error handling for unknowns

--- extract_covid_cases.py
+++ extract_covid_cases.py
@@ -15,10 +15,18 @@ def extract(state: str) -> (pd.DataFrame, list):
         print("String Needed")
         raise
     print("Reading Data for {}".format(state))
+    unknowns = {"Andaman and Nicobar Islands", "Assam", "Goa", "Manipur", "Sikkim", "Telangana"}
     # Read CSV file from covid19india API
-    data_districts = pd.read_csv("https://api.covid19india.org/csv/latest/districts.csv", header=None, usecols=[0, 1, 2, 3, 4, 5, 7])
+    if state in unknowns:
+        data_districts = pd.read_csv("https://api.covid19india.org/csv/latest/districts.csv", header=None,
+                                     usecols=[0, 1, 3, 4, 5, 7])
+    else:
+        data_districts = pd.read_csv("https://api.covid19india.org/csv/latest/districts.csv", header=None,
+                                     usecols=[0, 1, 2, 3, 4, 5, 7])
     # Get data of respective state
     data_districts_state = data_districts.loc[(data_districts[1] == state)]
-    districts = data_districts_state[2].unique()
+    districts = []
+    if state not in unknowns:
+    	districts = data_districts_state[2].unique()
     print(data_districts_state, districts)
     return data_districts_state, districts
